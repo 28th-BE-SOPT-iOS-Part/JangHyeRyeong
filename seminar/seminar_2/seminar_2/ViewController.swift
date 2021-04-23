07a794525f993cc9a5d5513679a3635d92530e7f
hryeong66 <jhr110326@gmail.com>
2021-04-23 22:40:20 +0900

    [Add] 2차 세미나 실습 추가

---
+++ seminar/seminar_2/seminar_2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  seminar_2
+//
+//  Created by 장혜령 on 2021/04/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
