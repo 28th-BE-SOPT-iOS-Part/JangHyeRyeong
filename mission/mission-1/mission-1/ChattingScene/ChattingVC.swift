0ce79f4b9de9aa52251a7c705733ceaaca589151
hryeong66 <jhr110326@gmail.com>
2021-04-23 22:43:54 +0900

    [Add] Chatting Scene 추가, Tabbar Item 변경

---
+++ mission/mission-1/mission-1/ChattingScene/ChattingVC.swift
@@ -0,0 +1,22 @@
+//
+//  ChattingVC.swift
+//  mission-1
+//
+//  Created by 장혜령 on 2021/04/23.
+//
+
+import UIKit
+
+class ChattingVC: UIViewController {
+
+    @IBOutlet weak var tabbarItem: UITabBarItem!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setClickedTabbarItem(tabItem: tabbarItem, ImageName: "message.fill")
+        
+    }
+    
+
+   
+
+}
