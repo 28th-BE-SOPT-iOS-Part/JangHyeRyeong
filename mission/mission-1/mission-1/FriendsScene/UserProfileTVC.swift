9b295e65c5f976e3cbe9b4337cd37af920e999f3
hryeong66 <jhr110326@gmail.com>
2021-05-07 16:30:33 +0900

    [ADD] 사용자프로필보여줄 Cell추가

---
+++ mission/mission-1/mission-1/FriendsScene/UserProfileTVC.swift
@@ -0,0 +1,23 @@
+//
+//  UserProfileTVC.swift
+//  mission-1
+//
+//  Created by 장혜령 on 2021/05/07.
+//
+
+import UIKit
+
+class UserProfileTVC: UITableViewCell {
+    static let identifier: String = "UserProfileTVC"
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
