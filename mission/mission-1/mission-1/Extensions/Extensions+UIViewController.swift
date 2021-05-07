f0cf36859a204a1922300aca8b4a931457743a7f
hryeong66 <jhr110326@gmail.com>
2021-05-07 16:28:23 +0900

    [CHORE] 전체 텝바에 추가했던 edgeInset 제거

--- mission/mission-1/mission-1/Extensions/Extensions+UIViewController.swift
+++ mission/mission-1/mission-1/Extensions/Extensions+UIViewController.swift
@@ -12,6 +12,6 @@ extension UIViewController{
     func setClickedTabbarItem(tabItem: UITabBarItem, ImageName: String){
         tabItem.selectedImage = UIImage(systemName: ImageName)
         tabItem.title = ""
-        tabItem.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)
+        //tabItem.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)
     }
 }
