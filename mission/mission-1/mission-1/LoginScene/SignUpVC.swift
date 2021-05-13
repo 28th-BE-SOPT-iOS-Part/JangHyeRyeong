f17c6ce3113418b4cacff02b33c808b6b931e3df
hryeong66 <jhr110326@gmail.com>
2021-05-13 13:06:14 +0900

    [CHORE] 회원가입시 서버를 통해 확인하는 코드 추가 tabbarview 연결부분은 아직 미완성

--- mission/mission-1/mission-1/LoginScene/SignUpVC.swift
+++ mission/mission-1/mission-1/LoginScene/SignUpVC.swift
@@ -17,23 +17,25 @@ class SignUpVC: UIViewController {
         super.viewDidLoad()
 
     }
+    
 
     @IBAction func makeNewAccount(_ sender: Any) {
         //비밀번호 제대로 쳤는지 확인
         
-        if checkAllText() == false{
+        if !checkAllText(){
             return
         }
+        checkPassword()
         
-        if let firstPW = passwordTextField.text,
-           let secondPW = checkPasswordTextField.text{
-            if firstPW != secondPW{
-                let alert = UIAlertController(title: "비밀번호 확인", message: "비밀번호가 맞지 않습니다.\n다시 입력해주시길 바랍니다.", preferredStyle: .alert)
-                let okAction = UIAlertAction(title: "확인", style: .default)
-                alert.addAction(okAction)
-                present(alert, animated: true, completion: nil)
-            }
-        }
+        self.makeRequestAlert(title: "알림",
+                             message: "회원가입을 하시겠습니까?",
+                             okAction: { _ in
+                               self.signUpAction()
+                             })
+
+        
+        
+       
         
         let storyboard = UIStoryboard(name: "Tabbar", bundle: nil)
         guard let nextVC = storyboard.instantiateViewController(identifier: "TabbarController") as? TabbarController else {
@@ -42,6 +44,34 @@ class SignUpVC: UIViewController {
         nextVC.modalPresentationStyle = .fullScreen
         present(nextVC, animated: true, completion: nil)
     }
+
+    
+    func signUpAction()
+    {
+        LoginService.shared.signUp(email: self.idTextField.text!, password: self.passwordTextField.text!) { result in
+            //print("email \(email) pw: \(password)" )
+            switch result
+            {
+            case .success(let message):
+                if let message = message as? String{
+                    self.makeAlert(title: "알림",
+                                   message: message)
+                }
+                
+            case .requestErr(let message):
+                
+                if let message = message as? String{
+                    
+                    self.makeAlert(title: "알림",
+                              message: message)
+                    
+                }
+            default :
+                print("ERROR")
+            }
+        }
+    }
+    
     
     func checkAllText() -> Bool{
         if idTextField.text == "" ||
@@ -52,6 +82,17 @@ class SignUpVC: UIViewController {
         return true
     }
     
+    func checkPassword() {
+        if let firstPW = passwordTextField.text,
+           let secondPW = checkPasswordTextField.text{
+            if firstPW != secondPW{
+                let alert = UIAlertController(title: "비밀번호 확인", message: "비밀번호가 맞지 않습니다.\n다시 입력해주시길 바랍니다.", preferredStyle: .alert)
+                let okAction = UIAlertAction(title: "확인", style: .default)
+                alert.addAction(okAction)
+                present(alert, animated: true, completion: nil)
+            }
+        }
+    }
     
     override func viewDidDisappear(_ animated: Bool) {
         self.navigationController?.popViewController(animated: true)
