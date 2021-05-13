d352e1bd3e5da111d6bddb0fa7c59471a505917c
hryeong66 <jhr110326@gmail.com>
2021-05-13 13:00:18 +0900

    [ADD] alert 관련 extentions 추가

--- mission/mission-1/mission-1/Extensions/Extensions+UIViewController.swift
+++ mission/mission-1/mission-1/Extensions/Extensions+UIViewController.swift
@@ -14,4 +14,48 @@ extension UIViewController{
         tabItem.title = ""
         //tabItem.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)
     }
+    
+    func makeRequestAlert(title : String,
+                           message : String,
+                           okAction : ((UIAlertAction) -> Void)?,
+                           cancelAction : ((UIAlertAction) -> Void)? = nil,
+                           completion : (() -> Void)? = nil)
+    {
+            
+        let generator = UIImpactFeedbackGenerator(style: .medium)
+        generator.impactOccurred()
+        
+        let alertViewController = UIAlertController(title: title, message: message,
+                                                    preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "확인", style: .default, handler: okAction)
+        alertViewController.addAction(okAction)
+        
+        
+        let cancelAction = UIAlertAction(title: "취소", style: .cancel, handler: cancelAction)
+        alertViewController.addAction(cancelAction)
+        
+
+        self.present(alertViewController, animated: true, completion: completion)
+    }
+        
+        
+    func makeAlert(title : String,
+                   message : String,
+                   okAction : ((UIAlertAction) -> Void)? = nil,
+                   completion : (() -> Void)? = nil)
+    {
+        let generator = UIImpactFeedbackGenerator(style: .medium)
+        generator.impactOccurred()
+        
+        let alertViewController = UIAlertController(title: title, message: message,
+                                                    preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "확인", style: .default, handler: okAction)
+        alertViewController.addAction(okAction)
+        
+        
+        self.present(alertViewController, animated: true, completion: nil)
+    }
+    
 }
