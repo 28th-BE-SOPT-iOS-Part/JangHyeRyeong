61c1ed72d6317280ac171e786f0a844af4438395
hryeong66 <jhr110326@gmail.com>
2021-05-13 13:03:14 +0900

    [ADD] 서버 URL 담는 struct 추가

---
+++ mission/mission-1/mission-1/Model/APIConstants.swift
@@ -0,0 +1,17 @@
+//
+//  APIConstants.swift
+//  mission-1
+//
+//  Created by 장혜령 on 2021/05/13.
+//
+
+import Foundation
+
+struct APIConstants{
+    static let baseURL = "http://cherishserver.com"
+    
+    static let loginURL = baseURL + "/login/signin"
+    static let signupURL = baseURL + "/login/signup"
+    
+    
+}
