fccee7f1b294cde066db3c822030930799179163
hryeong66 <jhr110326@gmail.com>
2021-05-13 13:05:19 +0900

    [CHORE] 서버통신을 통해 로그인 확인하는 코드 추가 tabbar view로 가는 부분은 아직 미완성

--- mission/mission-1/mission-1/LoginScene/ViewController.swift
+++ mission/mission-1/mission-1/LoginScene/ViewController.swift
@@ -14,12 +14,49 @@ class ViewController: UIViewController {
     @IBOutlet weak var subtitleLabel: UILabel!
     @IBOutlet weak var loginButton: UIButton!
     
+    var successCheck: Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setSubTitle()
     }
 
     @IBAction func touchUpLogin(_ sender: Any) {
+        self.makeRequestAlert(title: "알림",
+                                     message: "로그인을 하시겠습니까?",
+                                     okAction: { _ in
+                                       self.loginAction()
+                                     })
+        
+        }
+
+    func loginAction()
+    {
+        LoginService.shared.login(email: self.idTextField.text!, password: self.passwordTextField.text!) { result in
+            //print("email \(email) pw: \(password)" )
+            switch result
+            {
+            case .success(let message):
+                if let message = message as? String{
+                    
+                    self.makeAlert(title: "알림",
+                                   message: message)
+                }
+                
+            case .requestErr(let message):
+                
+                if let message = message as? String{
+                    
+                    self.makeAlert(title: "알림",message: message)
+                    
+                }
+            default :
+                print("ERROR")
+            }
+        }
+    }
+
+    func goToFriendView(){
         let storyboard = UIStoryboard(name: "Tabbar", bundle: nil)
         if idTextField.text != "" && passwordTextField.text != ""{
             guard let nextVC = storyboard.instantiateViewController(identifier: "TabbarController") as? TabbarController else {
@@ -28,6 +65,7 @@ class ViewController: UIViewController {
             nextVC.modalPresentationStyle = .fullScreen
             self.present(nextVC, animated: true, completion: nil)
         }
+        
     }
     
     @IBAction func makeNewAccount(_ sender: Any) {
