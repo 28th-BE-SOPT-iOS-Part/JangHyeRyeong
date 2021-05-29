116322c2aef52c8bb1a23d965917a0b06ac65267
hryeong66 <jhr110326@gmail.com>
2021-05-29 11:11:08 +0900

    [ADD] 7주차 프로젝트 생성

---
+++ seminar/seminar_7/seminar_7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  seminar_7
+//
+//  Created by 장혜령 on 2021/05/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
